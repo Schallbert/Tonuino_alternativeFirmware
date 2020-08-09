4652c3da6ecf7e4330efc66a6a2608458d6f82f4
Schallbert <schal@mailbox.org>
2020-08-09 14:23:18 +0200

    change keepAlive behavior to make a difference between shutdown request shutdown allow (perform)

--- src/interface_KeepAlive/interface_KeepAlive.cpp
+++ src/interface_KeepAlive/interface_KeepAlive.cpp
@@ -1,43 +1,61 @@
 #include "interface_KeepAlive.h"
 
 //Interface implementation for active low TTL/Relay logic
-KeepAlive::KeepAlive(uint8_t pinID, bool pinActiveState, uint16_t seconds)
+KeepAlive::KeepAlive(uint8_t ui8PinID,
+                     bool bPinActiveState,
+                     uint16_t ui16Seconds) : m_ui8PinID(ui8PinID),
+                                             m_bPinActiveState(bPinActiveState),
+                                             m_ui16Seconds(ui16Seconds)
 {
-    this->pinID = pinID;
-    this->pinActiveState = pinActiveState;
-    pinMode(this->pinID, OUTPUT);
-    digitalWrite(pinID, pinActiveState);
+    // Keep alive command right at constructor call
+    pinMode(m_ui8PinID, OUTPUT);
+    digitalWrite(m_ui8PinID, m_bPinActiveState);
 }
 
 void KeepAlive::keep_alive()
 {
-    digitalWrite(pinID, pinActiveState);
+    digitalWrite(m_ui8PinID, m_bPinActiveState);
 }
 
-void KeepAlive::shut_down()
+void KeepAlive::request_shutdown()
 {
-    digitalWrite(pinID, !pinActiveState);
+    m_bShutDownRequested = true;
 }
 
-void KeepAlive::set_idle_timer(bool idleTimerActive)
+void KeepAlive::allow_shutdown(bool bAllow)
 {
-    this->idleTimerActive = idleTimerActive;
-}
-
-void KeepAlive::idle_timer_tick1ms()
-{
-    if (idleTimerActive)
+    if (bAllow)
     {
-        ++tick1ms;
-        if (tick1ms / 1000 >= seconds)
+        if (m_bShutDownRequested)
         {
-            tick1ms = 0;
-            idleTimerActive = false;
-            shut_down();
+            digitalWrite(m_ui8PinID, !m_bPinActiveState);
         }
     }
     else
     {
-        tick1ms = 0;
+        m_bShutDownRequested = false;
+    }
+}
+
+void KeepAlive::set_idle_timer(bool bIdleTimerActive)
+{
+    m_bIdleTimerActive = bIdleTimerActive;
+}
+
+void KeepAlive::idle_timer_tick1ms()
+{
+    if (m_bIdleTimerActive)
+    {
+        ++m_ui32Tick1ms;
+        if (m_ui32Tick1ms / 1000 >= m_ui16Seconds)
+        {
+            m_ui32Tick1ms = 0;
+            m_bIdleTimerActive = false;
+            m_bShutDownRequested = true;
+        }
+    }
+    else
+    {
+        m_ui32Tick1ms = 0;
     }
 }
--- src/interface_KeepAlive/interface_KeepAlive.h
+++ src/interface_KeepAlive/interface_KeepAlive.h
@@ -8,23 +8,29 @@ class KeepAlive
     /*  KeepAlive keeps the microcontroller and its periphery running
         until requested to shut down by the main program,
         e.g. after a timeout. As a KeepAlive function electronically
-        can be designed in many sensible ways (TTL, Relay, Hi/Lo-Active etc.)
-        this interface definition is kept abstract.
+        can be designed in many sensible ways (TTL, Relay, Hi/Lo-Active etc.).
     */
 public:
     //Note: For usage of a bistable relay, active must be inverted!
     KeepAlive(uint8_t pinID, bool pinActiveState, uint16_t seconds);
 
+// Physically keeps system powered (depending on external cirtuitry)
     void keep_alive();
-    void shut_down();
+    // Requests shut down to keep alive system; will be evaluated when "allow" method is called
+    void request_shutdown();
+    // Will shut down once bAllow is TRUE and shutdown has been requested
+    void allow_shutdown(bool bAllow);
+    // method to be attached to a 1ms interrupt to count system idle time
     void idle_timer_tick1ms();
-    void set_idle_timer(bool active);
+    // setter method to request the idle timer to (re)start
+    void set_idle_timer(bool bIdleTimerActive);
 
 private:
-    uint8_t pinID;
-    bool pinActiveState;
-    bool idleTimerActive;
-    uint16_t seconds;
-    volatile uint32_t tick1ms;
+    uint8_t m_ui8PinID;
+    bool m_bShutDownRequested{false};
+    bool m_bPinActiveState{true};
+    bool m_bIdleTimerActive{false};
+    uint16_t m_ui16Seconds{0};
+    volatile uint32_t m_ui32Tick1ms{0};
 };
 #endif //KEEPALIVE_H
