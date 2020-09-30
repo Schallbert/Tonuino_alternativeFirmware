a6096d71afa8fcb3eeb1fd3eb8eba1f4d64611ac
Schallbert <schal@mailbox.org>
2020-09-30 13:01:55 +0200

    outsourced static NfcTag_factory methods. Compiles now.

--- lib/NfcTag/NfcTag_factory.h
+++ lib/NfcTag/NfcTag_factory.h
@@ -7,40 +7,11 @@
 
 class NfcTag_factory
 {
-
 public:
     // Returns a de-facto singleton of correct tag-Type
-    static NfcTag_interface *getInstance(MFRC522_interface::eTagType tagType, MFRC522_interface *pMfrc522)
-    {
-        removeInstance(); // remove "old" instance if necessary
-        switch (tagType)
-        {
-        case MFRC522_interface::PICC_TYPE_MIFARE_MINI:
-            m_pNfcTag = new NfcTag_MifareMini1k4k(pMfrc522);
-            break;
-        case MFRC522_interface::PICC_TYPE_MIFARE_1K:
-            m_pNfcTag = new NfcTag_MifareMini1k4k(pMfrc522);
-            break;
-        case MFRC522_interface::PICC_TYPE_MIFARE_4K:
-            m_pNfcTag = new NfcTag_MifareMini1k4k(pMfrc522);
-            break;
-        case MFRC522_interface::PICC_TYPE_MIFARE_UL:
-            m_pNfcTag = new NfcTag_MifareUltralight(pMfrc522);
-            break;
-        default:
-            m_pNfcTag = nullptr;
-        }
-        return m_pNfcTag;
-    }
-
-    static void removeInstance()
-    {
-        if (m_pNfcTag)
-        {
-            delete m_pNfcTag;
-        }
-    }
-
+    static NfcTag_interface *getInstance(MFRC522_interface::eTagType tagType, MFRC522_interface *pMfrc522);
+    static void removeInstance();
+  
 private:
     static NfcTag_interface *m_pNfcTag;
 };
