2fd327c252177091d1ccaaaa2045ca5feed40595
Schallbert <schal@mailbox.org>
2020-10-17 15:22:15 +0200

    complete unittest for EncoderLongPress

--- test/desktop/unittest_EncoderLongPress.cpp
+++ test/desktop/unittest_EncoderLongPress.cpp
@@ -38,9 +38,50 @@ TEST_F(EncoderLongPressRepeatTest, encoderServiceCalled)
 TEST_F(EncoderLongPressRepeatTest, getButton_Open_willNeverReturnLongPressRepeat)
 {
     ON_CALL(enc, getButton()).WillByDefault(Return(ClickEncoder_interface::Open));
+    for (uint8_t i = 0; i < longPressRepeatTicks + 10; ++i)
+    {
+        m_pEnc->service();
+    }
+    ASSERT_EQ(m_pEnc->getButton(), Encoder_longPressRepeat::Open);
+}
+
+TEST_F(EncoderLongPressRepeatTest, getButton_Held_notLongEnough_wilReturnHeld)
+{
+    ON_CALL(enc, getButton()).WillByDefault(Return(ClickEncoder_interface::Held));
+    for (uint8_t i = 0; i < longPressRepeatTicks; ++i)
+    {
+        m_pEnc->service();
+    }
+    ASSERT_EQ(m_pEnc->getButton(), Encoder_longPressRepeat::Held);
+}
+
+TEST_F(EncoderLongPressRepeatTest, getButton_Held_justLongEnough_wilReturnLongPressRepeat)
+{
+    ON_CALL(enc, getButton()).WillByDefault(Return(ClickEncoder_interface::Held));
     for (uint8_t i = 0; i < longPressRepeatTicks + 1; ++i)
     {
         m_pEnc->service();
     }
-    ASSERT_EQ(m_pEnc->getButton(), ClickEncoder_interface::Open);
+    ASSERT_EQ(m_pEnc->getButton(), Encoder_longPressRepeat::LongPressRepeat);
+}
+
+TEST_F(EncoderLongPressRepeatTest, getButton_Held_longerThanNeeded_wilReturnLongPressRepeat)
+{
+    ON_CALL(enc, getButton()).WillByDefault(Return(ClickEncoder_interface::Held));
+    for (uint8_t i = 0; i < longPressRepeatTicks + 5; ++i)
+    {
+        m_pEnc->service();
+    }
+    ASSERT_EQ(m_pEnc->getButton(), Encoder_longPressRepeat::LongPressRepeat);
+}
+
+TEST_F(EncoderLongPressRepeatTest, getButton_Held_longPressIsResetOnRead)
+{
+    ON_CALL(enc, getButton()).WillByDefault(Return(ClickEncoder_interface::Held));
+    for (uint8_t i = 0; i < longPressRepeatTicks + 1; ++i)
+    {
+        m_pEnc->service();
+    }
+    m_pEnc->getButton();
+    ASSERT_EQ(m_pEnc->getButton(), Encoder_longPressRepeat::Held);
 }
