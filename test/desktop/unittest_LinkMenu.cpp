24c6a95be421fabf763ad9a1afcda3628a196d5d
Schallbert <schal@mailbox.org>
2020-11-13 20:34:08 +0100

    reactivate some more tests.

--- test/desktop/unittest_LinkMenu.cpp
+++ test/desktop/unittest_LinkMenu.cpp
@@ -44,6 +44,11 @@ MATCHER(invalidFolder, "")
     return (!arg.is_initiated());
 }
 
+MATCHER(validFolder, "")
+{
+    return (arg.is_initiated());
+}
+
 MATCHER_P(identicalFolder, comp, "")
 {
     return ((arg.get_folder_id() == comp.get_folder_id()) &&
@@ -221,6 +226,70 @@ TEST_F(LinkMenuTest, selectPlayMode_testRollover_getPrompt_returnsSAVEPROGRESS)
     linkMenu->handlePlayback();
 }
 
+TEST_F(LinkMenuTest, noInit_abort_noPromptSet)
+{
+    VoicePrompt expect{};
+    expect.promptId = MSG_ABORTED;
+    expect.allowSkip = true;
+
+    linkMenu->abort();
+
+    EXPECT_CALL(m_promptPlayerMock, playPrompt(identicalPrompt(expect)));
+    linkMenu->handlePlayback();
+}
+
+TEST_F(LinkMenuTest, folderId1Selected_abort_promptsAborted)
+{
+    VoicePrompt expect{};
+    expect.promptId = MSG_ABORTED;
+    expect.allowSkip = true;
+
+    linkMenu->confirm();
+    linkMenu->selectNext();
+
+    linkMenu->abort();
+
+    EXPECT_CALL(m_promptPlayerMock, playPrompt(identicalPrompt(expect)));
+    linkMenu->handlePlayback();
+}
+
+TEST_F(LinkMenuTest, folderIdAndPlayModeSelected_abort_promptsAborted)
+{
+    VoicePrompt expect{};
+    expect.promptId = MSG_ABORTED;
+    expect.allowSkip = true;
+
+    linkMenu->confirm();
+    linkMenu->selectNext();
+    linkMenu->confirm();
+    linkMenu->selectNext();
+
+    linkMenu->abort();
+
+    EXPECT_CALL(m_promptPlayerMock, playPrompt(identicalPrompt(expect)));
+    linkMenu->handlePlayback();
+}
+
+TEST_F(LinkMenuTest, menuCompleted_abort_promptsAborted)
+{
+    VoicePrompt expect{};
+    expect.promptId = MSG_ABORTED;
+    expect.allowSkip = true;
+
+    linkMenu->confirm();
+    linkMenu->selectNext();
+    linkMenu->confirm();
+    linkMenu->selectNext();
+    linkMenu->confirm();
+
+    linkMenu->abort();
+
+    EXPECT_CALL(m_promptPlayerMock, playPrompt(identicalPrompt(expect)));
+    linkMenu->handlePlayback();
+}
+
+// Folder Preview Tests
+
 TEST_F(LinkMenuTest, noInit_getPreview_returnsInvalidFolder)
 {
     EXPECT_CALL(m_promptPlayerMock, playFolderPreview(invalidFolder()));
@@ -265,31 +334,26 @@ TEST_F(LinkMenuTest, selectFolderId1_getPreview_returnsPreviewFolder1)
     linkMenu->handlePlayback();
 }
 
-#if 0
 TEST_F(LinkMenuTest, selectFolderIdMAX_getPreview_returnsPreviewFolderMAX)
 {
+    Folder expect{Folder(MAXFOLDERCOUNT, Folder::ONELARGETRACK, 1)};
+
     linkMenu->confirm();
     linkMenu->selectPrev();
 
-    Folder testFolder = linkMenu->getPreview();
-
-    ASSERT_EQ(testFolder.get_folder_id(), MAXFOLDERCOUNT);
-    ASSERT_EQ(testFolder.get_play_mode(), Folder::ONELARGETRACK);
-    ASSERT_EQ(testFolder.get_track_count(), 1);
+    EXPECT_CALL(m_promptPlayerMock, playFolderPreview(identicalFolder(expect)));
+    linkMenu->handlePlayback();
 }
 
-TEST_F(LinkMenuTest, selectFolderId1_confirm_getPreview_returnsUndefinedPlaymode)
+TEST_F(LinkMenuTest, selectFolderId1_confirm_getPreview_returnsInvalidFolder)
 {
     // enter, select folderId1, saveFolderId
     linkMenu->confirm();
     linkMenu->selectNext();
     linkMenu->confirm();
 
-    Folder testFolder = linkMenu->getPreview();
-
-    ASSERT_EQ(testFolder.get_folder_id(), 1);
-    ASSERT_EQ(testFolder.get_play_mode(), Folder::UNDEFINED);
-    ASSERT_EQ(testFolder.get_track_count(), 1);
+    EXPECT_CALL(m_promptPlayerMock, playFolderPreview(invalidFolder()));
+    linkMenu->handlePlayback();
 }
 
 TEST_F(LinkMenuTest, selectFolderIdAndPlayMode_getPreview_returnsInvalidFolder)
@@ -300,12 +364,11 @@ TEST_F(LinkMenuTest, selectFolderIdAndPlayMode_getPreview_returnsInvalidFolder)
     linkMenu->confirm();
     linkMenu->selectNext();
 
-    Folder testFolder = linkMenu->getPreview();
-
-    ASSERT_FALSE(testFolder.is_valid());
+    EXPECT_CALL(m_promptPlayerMock, playFolderPreview(invalidFolder()));
+    linkMenu->handlePlayback();
 }
 
-TEST_F(LinkMenuTest, confirmFolderIdAndPlayMode_getPreview_returnsValidFolder)
+TEST_F(LinkMenuTest, confirmFolderIdAndPlayMode_getPreview_returnsInvalidFolder)
 {
     // enter, select folderId1, saveFolderId
     linkMenu->confirm();
@@ -314,53 +377,12 @@ TEST_F(LinkMenuTest, confirmFolderIdAndPlayMode_getPreview_returnsValidFolder)
     linkMenu->selectNext();
     linkMenu->confirm();
 
-    Folder testFolder = linkMenu->getPreview();
-
-    ASSERT_TRUE(testFolder.is_valid());
+    EXPECT_CALL(m_promptPlayerMock, playFolderPreview(invalidFolder()));
+    linkMenu->handlePlayback();
 }
 
-TEST_F(LinkMenuTest, noInit_abort_noPromptSet)
-{
-    linkMenu->abort();
-
-    ASSERT_EQ((linkMenu->getPrompt()).promptId, MSG_ABORTED);
-}
-
-TEST_F(LinkMenuTest, folderId1Selected_abort_promptsAborted)
-{
-    linkMenu->confirm();
-    linkMenu->selectNext();
-
-    linkMenu->abort();
-
-    ASSERT_EQ((linkMenu->getPrompt()).promptId, MSG_ABORTED);
-}
-
-TEST_F(LinkMenuTest, folderIdAndPlayModeSelected_abort_promptsAborted)
-{
-    linkMenu->confirm();
-    linkMenu->selectNext();
-    linkMenu->confirm();
-    linkMenu->selectNext();
-
-    linkMenu->abort();
-
-    ASSERT_EQ((linkMenu->getPrompt()).promptId, MSG_ABORTED);
-}
-
-TEST_F(LinkMenuTest, menuCompleted_abort_promptsAborted)
-{
-    linkMenu->confirm();
-    linkMenu->selectNext();
-    linkMenu->confirm();
-    linkMenu->selectNext();
-    linkMenu->confirm();
-
-    linkMenu->abort();
-
-    ASSERT_EQ((linkMenu->getPrompt()).promptId, MSG_ABORTED);
-}
-
+//////////////////////////////////////
+#if 0
 TEST_F(LinkMenuTest, noInit_abort_getPreview_returnsInvalidFolder)
 {
     linkMenu->abort();
