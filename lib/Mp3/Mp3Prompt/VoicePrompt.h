6258bd24df239358678fd40a3e7e7ea7a7d95ab0
Schallbert <schal@mailbox.org>
2021-03-16 20:17:52 +0100

    add and sort voicePrompts

--- lib/Mp3/Mp3Prompt/VoicePrompt.h
+++ lib/Mp3/Mp3Prompt/VoicePrompt.h
@@ -9,20 +9,24 @@ public:
     //Tonuino mp3 prompt IDs, MUST BE KEPT IN SYNC WITH FILE NAMES on SD CARD!
     enum ePromptIds
     {
+        MSG_SELECT_FOLDERID = 300,
+        MSG_SELECT_PLAYMODE = 400,
+        MSG_PLAYMODE_ALBUM = 401,
+        MSG_PLAYMODE_RANDOM = 402,
+        MSG_PLAYMODE_SAVEPROGRESS = 403,
+        MSG_PLAYMODE_ONELARGETRACK = 404,
         MSG_ERROR_CARDWRITE = 410,
         MSG_ERROR_CARDREAD = 411,
         MSG_ERROR_FOLDER = 412,
         MSG_ERROR_TOOMANYTRACKS = 413,
+        MSG_HELP = 420, // TODO HOW TO SELECT UserInput variant?
         MSG_BUTTONLOCK = 426,
         MSG_BUTTONFREE = 427,
         MSG_STARTUP = 500,
         MSG_SHUTDOWN = 501,
-        MSG_SELECT_FOLDERID = 300,
-        MSG_SELECT_PLAYMODE = 400,
-        MSG_HELP = 420, // TODO HOW TO SELECT UserInput variant?
-        MSG_TAGCONFSUCCESS = 702,
         MSG_DELETETAG = 700,
         MSG_CONFIRM_DELETION = 701,
+        MSG_TAGCONFSUCCESS = 702,
         MSG_ABORTED = 802
     };
 
